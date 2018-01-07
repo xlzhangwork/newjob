fa2b0f46771475e6a20a2f9971b43a589c4bda4e
superman <15244603097@163.com>
2018-01-07 20:44:00 +0800

    尝试一下

--- src/main/java/com/xlz/demo/FirstTest.java
+++ src/main/java/com/xlz/demo/FirstTest.java
@@ -3,6 +3,7 @@ package com.xlz.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,10 +19,11 @@ public class FirstTest {
     @Value("${girl.cup}")
     private String cup;
 
-    @RequestMapping(value =  {"/hello","/hi"}, method = RequestMethod.POST)
-    public String sayHello(){
+    @RequestMapping(value =  {"/hello","/hi/{id}"}, method = RequestMethod.GET)
+    public String sayHello(@PathVariable("id") Integer id){
         System.out.println("Hello" + "\n");
         System.out.println(girlProperties.getAge() + cup);
-        return girlProperties.getCup();
+        return "id: " + id;
+        //return girlProperties.getCup();
     }
 }
