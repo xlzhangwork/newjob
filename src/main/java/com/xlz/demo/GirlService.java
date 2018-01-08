e9f6b816e5ac0aea090e02c23ad324272504910b
superman <15244603097@163.com>
2018-01-08 23:08:17 +0800

    springboot入门

--- src/main/java/com/xlz/demo/GirlService.java
+++ src/main/java/com/xlz/demo/GirlService.java
@@ -3,6 +3,8 @@ package com.xlz.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 /**
  * @Author: xlzhang
  * @Description:
@@ -15,6 +17,7 @@ public class GirlService {
     @Autowired
     private GirlRepository girlRepository;
 
+    @Transactional
     public void insetTwo(){
         Girl girlA = new Girl();
         girlA.setCupSize("B");
@@ -22,7 +25,7 @@ public class GirlService {
         girlRepository.save(girlA);
 
         Girl girlB = new Girl();
-        girlB.setCupSize("D");
+        girlB.setCupSize("DD");
         girlB.setAge(18);
         girlRepository.save(girlB);
     }
